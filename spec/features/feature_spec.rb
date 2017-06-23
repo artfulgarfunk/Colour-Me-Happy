1615793bf31c3d0e264673a9a40ff25a8bad097f
Jack <92jackhenderson@gmail.com>
2017-06-23 10:54:02 +0100

    Test update

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -15,20 +15,16 @@ feature 'Welcome' do
 
   context "where the selected colour is used as a filter along with a random serach term" do
     it "displays a welcome message based on what colour you selected" do
-      visit('/')
-      click_link_or_button "pink"
+      visit('/results?colour=green&noun=water')
+      expect(page).to have_content 'Top results for green and water'
       expect(page).to have_content 'Colourful Results...'
-      expect(page).to have_content "Top 5 results for"
-      expect(page).to have_content "with a pink filter"
     end
 
     it "displays the top 5 image results" do
       visit('/results?colour=blue&noun=happiness')
-      expect(page).to have_content 'Top 5 results for happiness with a blue filter'
       expect(page).to have_selector('img')
       expect(page).to have_link("")
     end
-
   end
 
 end
